78b6a133bc261764945c95979a42cead0809269e
tati <ost11tan@gmail.com>
2022-08-23 02:30:18 +0300

    Example003 if

---
+++ Example005_if/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите ваше имя");
+string UserName = Console.ReadLine( );
+if (UserName.ToLower()=="таня")
+{
+    Console.WriteLine("Привет,Конфеточка!!! ");
+}
+else
+{
+  Console.WriteLine(UserName);  
+}
+
