d6a009c8e392b17a396a9d590b646130d7b8ffa8
tati <ost11tan@gmail.com>
2022-08-26 03:15:28 +0300

    заполнение массивов

---
+++ lesson2/Example002/Program.cs
@@ -0,0 +1,14 @@
+﻿int [] array={67,15,62,55,218,7,9};
+int n = array.Length;
+int find = 4;
+
+int index = 0;
+
+while (index < n)
+{
+   if (array[index]== find)
+   {
+    Console.WriteLine(index);
+   } 
+   index++;
+}
